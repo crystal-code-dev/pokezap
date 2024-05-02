621ecef66f866995314bab9d72a07c6e25602bd0
wayTo <yurigarciadev@gmail.com>
2024-05-02 17:00:20 -0300

    raidroom route nameset

---
+++ packages/core/src/constants/route/routeNames.ts
@@ -0,0 +1,13 @@
+export const firstNames = ['montanha', 'garra', 'flor', 'aurora', 'luz', 'canção', 'miragem', 'água', 'rocha']
+
+export const secondNames = [
+  'alva',
+  'escura',
+  'carmesim',
+  'radiante',
+  'misteriosa',
+  'encantada',
+  'valente',
+  'verdadeira',
+  'cristalina',
+]
