716d9ea67ff79cc45223486c08d54bdafc89f7a9
wayTo <yurigarciadev@gmail.com>
2024-05-02 13:07:48 -0300

    maintenance adm router

---
+++ packages/core/src/infra/routes/admRoutes/maintenance.ts
@@ -0,0 +1,56 @@
+import prisma from '../../../../../prisma-provider/src'
+import { groupChatNameUpdate } from '../../../server/helpers/groupChatNameUpdate'
+import { IResponse } from '../../../server/models/IResponse'
+import { TRouteParams } from '../router'
+
+export const maintenance = async (data: TRouteParams): Promise<IResponse> => {
+  const [, , , onOff] = data.routeParams
+
+  const maintenanceOn = onOff === 'ON'
+
+  const gameRooms = await prisma.gameRoom.findMany({
+    where: {
+      phone: {
+        contains: '@g.us',
+      },
+    },
+    select: {
+      name: true,
+      phone: true,
+    },
+  })
+
+  const promises = []
+
+  if (maintenanceOn) {
+    for (const gameRoom of gameRooms) {
+      const newName = '[🛠-OFF]' + gameRoom.name
+      promises.push(
+        groupChatNameUpdate({
+          chatId: gameRoom.phone,
+          newName,
+        })
+      )
+    }
+  } else {
+    for (const gameRoom of gameRooms) {
+      const newName = gameRoom.name.replace('[🛠-OFF]', '').replace('[🛠-OFF]', '').replace('[🛠-OFF]', '')
+
+      promises.push(
+        groupChatNameUpdate({
+          chatId: gameRoom.phone,
+          newName,
+        })
+      )
+    }
+  }
+
+  await Promise.all(promises)
+
+  return {
+    message: ``,
+    react: '✔',
+    status: 200,
+    data: null,
+  }
+}
